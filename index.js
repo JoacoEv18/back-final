dd0e87faf9014a744e17e6efa1d563854c2df169
Joaco <joaquineliasvega@gmail.com>
2023-01-03 20:00:01 -0300

    Deploy Proyecto Final

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 9000;
+require('./database/conexion');
+const path = require('path');
+const multer = require('multer');
+const app = express();
+const personaController = require('./controllers/personaController');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors());
+app.use(morgan('common'));
+
+
+//Configuramos Multer
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        console.log(file);
+        callback(null, 'uploads')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${file.originalname}`)
+    }
+});
+
+//Pasamos la config a Multer
+const uploads = multer({storage: storage}) 
+
+
+app.get('/datos', async (req, res) => {
+    res.json({
+        personas: await personaController.findAll()
+    });
+});
+
+app.post('/crear', async (req, res) => {
+    const {nombre, apellido, dni} = req.body;
+    console.log(`${nombre} ${apellido}, ${dni}`);
+    await personaController.create(req.body)
+    res.send('Persona Creada')
+});
+
+app.delete('/:id', (req, res) => {
+    res.send('Persona Eliminada')
+});
+
+app.put('/:id', (req, res) => {
+    res.send('Persona Actualizada')
+});
+
+app.post('/subir', uploads.single('miArchivo') , (req, res, next) => {
+    const file = req.file;
+    if(!file){
+        const error = new Error('Error subiendo el Archivo')
+        error.httpStatusCode=404
+        return next(error)
+    }
+    res.send(`Archivo <b>${file.originalname}</b> subido correctamente`)
+});
+
+
+app.listen(PORT, () => {
+    console.log(`MERN trabajando en el Puerto ${PORT}`);
+});
